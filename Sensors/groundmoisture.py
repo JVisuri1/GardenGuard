5023919c77ba376ac72ea362d14e0a6027f5f2e9
JVisuri1 <34973409+JVisuri1@users.noreply.github.com>
2021-02-21 17:58:53 +0200

    123

--- Sensors/groundmoisture.py
+++ Sensors/groundmoisture.py
@@ -1,26 +1,32 @@
-import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
-import time # This is the time library, we need this so we can use the sleep function
+import RPi.GPIO as GPIO
+import time
 
-# Set our GPIO numbering to BCM
-GPIO.setmode(GPIO.BCM)
+class GroundMoistureSensor:
+	def __init__(self,channel):
+		self.channel = channel
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.channel, GPIO.IN)
+		
+	def getSensorValue(self):
+		if GPIO.input(self.channel):
+			return 0
+		else:
+			return 1
 
-# Define the GPIO pin that we have our digital output from our sensor connected to
-channel = 17
-# Set the GPIO pin to an input
-GPIO.setup(channel, GPIO.IN)
 
-def callback(channel):  
-	if GPIO.input(channel):
-		print("LED off")
-	else:
-		print("LED on")
+
+#def callback(channel):  
+#	if GPIO.input(channel):
+#		print("LED off")
+#	else:
+#		print("LED on")
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+#GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-GPIO.add_event_callback(channel, callback)
+#GPIO.add_event_callback(channel, callback)
 
 # This is an infinte loop to keep our script running
-while True:
-	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
-	time.sleep(0.1)
+
+
+
--- gardenguard.py
+++ gardenguard.py
@@ -1,15 +1,24 @@
 from configparser import ConfigParser
-from datetime import datetime
+import time # This is the time library, we need this so we can use the sleep function
+from Sensors.groundmoisture import GroundMoistureSensor
 
-from mysql.connector.dbapi import Date
-from SQL.sqlclient import SqlClient
+import mysql.connector
 
+while True:
+    config = ConfigParser()
+    config.read('config.ini')
+    dbsettings = config["database"]
+    conn = mysql.connector.connect(
+    host=dbsettings["host"],
+    user=dbsettings["user"],
+    passwd=dbsettings["pass"],
+    database=dbsettings["db"])
+    cursor = conn.cursor()
+    sensor1=GroundMoistureSensor(17)
+    value1 = sensor1.getSensorValue()
 
-config = ConfigParser()
-config.read('config.ini')
-
-dbsettings = config["database"]
-
-dbclient = SqlClient(dbsettings["host"], dbsettings["user"], dbsettings["pass"], dbsettings["db"])
-
-print(dbclient.readPaged(datetime.today(), datetime.today(), 1, 10))
+    cursor.execute("INSERT INTO sensor_log (temp, humidity, moist1, moist2) VALUES ({},{},{},{})".format(0, 0, value1, 0))
+    conn.commit()
+    print(cursor.rowcount, "record inserted.")
+    
+    time.sleep(5)
