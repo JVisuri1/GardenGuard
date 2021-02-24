61cc86ad7922c70c4f829dee52db8749eecf8736
JVisuri1 <34973409+JVisuri1@users.noreply.github.com>
2021-02-24 21:19:23 +0200

    3

---
+++ Sensors/air.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_dht
+
+class AirSensor:
+
+    def getTemperature(self, dhtDevice):
+        try:
+            return dhtDevice.temperature
+        except RuntimeError as error:
+            print(error.args[0])
+            return 0
+
+    def getHumidity(self, dhtDevice):
+        try:
+            return dhtDevice.humidity
+        except RuntimeError as error:
+            print(error.args[0])
+            return 0
--- gardenguard.py
+++ gardenguard.py
@@ -1,3 +1,4 @@
+from Sensors.air import AirSensor
 from configparser import ConfigParser
 import time # This is the time library, we need this so we can use the sleep function
 from Sensors.groundmoisture import GroundMoistureSensor
@@ -24,11 +25,9 @@ while True:
     sensor1 = GroundMoistureSensor(17)
     groundMoist1 = sensor1.getSensorValue()
 
-    try:
-        temp = dhtDevice.temperature
-        humidity = dhtDevice.humidity
-    except RuntimeError as error:
-        print(error.args[0])
+    airSensor = AirSensor()
+    temp = airSensor.getTemperature(dhtDevice)
+    humidity = airSensor.getHumidity(dhtDevice)
 
     cursor.execute("INSERT INTO sensor_log (temp, humidity, moist1, moist2) VALUES ({:.1f},{:.1f},{},{})".format(temp, humidity, groundMoist1, 0))
     conn.commit()
